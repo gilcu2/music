c1f798b43c3c8d3fb1a82564c97417af01b99e84
Reynaldo Gil <gilcu2@gmail.com>
2019-09-18 20:20:49 +0100

    reading tsc for test working

--- src/main/scala/com/gilcu2/processing/Processing.scala
+++ src/main/scala/com/gilcu2/processing/Processing.scala
@@ -1,26 +1,29 @@
 package com.gilcu2.processing
 
-import org.apache.spark.sql.types.{StringType, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Processing {
 
-  val userIdField="userId"
-  val timeField="timeStamp"
-  val artistIdField="artistId"
-  val artistNameField="artistName"
-  val songIdField="songId"
-  val songName="songName"
+  val userIdField = "userId"
+  val timeField = "timeStamp"
+  val artistIdField = "artistId"
+  val artistNameField = "artistName"
+  val songIdField = "songId"
+  val songName = "songName"
 
-  val fields=Array(userIdField,timeField,artistIdField,artistNameField,songIdField,songName)
+  val fields = Array(userIdField, timeField, artistIdField, artistNameField, songIdField, songName)
 
-  def prepareData(df:DataFrame)(implicit spark:SparkSession):DataFrame={
-
-
+  def prepareData(df: DataFrame)(implicit spark: SparkSession): DataFrame =
     df
-  }
+      .withColumnRenamed("_c0", userIdField)
+      .withColumnRenamed("_c1", timeField)
+      .withColumnRenamed("_c2", artistIdField)
+      .withColumnRenamed("_c3", artistNameField)
+      .withColumnRenamed("_c4", songIdField)
+      .withColumnRenamed("_c5", songName)
 
-  def computeTopFromLongestSessions(tracks:DataFrame, top:Int,sessions:Int)(implicit spark:SparkSession):DataFrame={
+
+  def computeTopFromLongestSessions(tracks: DataFrame, top: Int, sessions: Int)(implicit spark: SparkSession): DataFrame = {
     spark.emptyDataFrame
   }
 
