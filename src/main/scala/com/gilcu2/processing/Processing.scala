632d6c3905043ca6a6bb64b45d5fbfd2f9bbb513
Reynaldo Gil <gilcu2@gmail.com>
2019-09-19 05:55:28 +0100

    beginning putting interesting track info in the array

--- src/main/scala/com/gilcu2/processing/Processing.scala
+++ src/main/scala/com/gilcu2/processing/Processing.scala
@@ -1,7 +1,7 @@
 package com.gilcu2.processing
 
-import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 
 object Processing {
 
@@ -23,15 +23,21 @@ object Processing {
       .withColumnRenamed("_c4", songIdField)
       .withColumnRenamed("_c5", songName)
 
-  def getSessions(userTracks:Column):Column=
-    size(userTracks)
-
   def computeLongestSessions(tracks: DataFrame, sessions: Int)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
-    tracks
+    val userTracks = tracks
       .groupBy(userIdField)
-      .agg(sort_array(collect_list(timeField)).as("user_tracks"))
-      .select(col(userIdField),getSessions($"user_tracks"))
+      .agg(sort_array(collect_list(concat(col(timeField),col(artistNameField),col(songName)))).as("user_tracks"))
+    userTracks.printSchema()
+    userTracks.show(20,truncate = 80,vertical = true)
+
+    userTracks.select(col(userIdField), getSessions($"user_tracks"))
+  }
+
+  def getSessions(userTracks: Column): Column = {
+
+    val len = size(userTracks)
+    len
   }
 
   def computeTopFromLongestSessions(tracks: DataFrame, top: Int, sessions: Int)(implicit spark: SparkSession): DataFrame = {
